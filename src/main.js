fc22bbcd89009ce376e9c11daacffd0e30c7ef60
AndrewLEEJM <woals214@gmail.com>
2021-01-18 17:05:06 +0900

    다운로드 기능 생성

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import BootstrapVue from 'bootstrap-vue';
+import VueHtml2Canvas from 'vue-html2canvas';
 import App from './App.vue';
 import router from './router';
 import store from './store';
@@ -7,6 +8,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 
 Vue.use(BootstrapVue);
+Vue.use(VueHtml2Canvas);
 Vue.config.productionTip = false;
 
 new Vue({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="letter_container">
-    <div class="letter_back">
-      <p v-html="letterText">
+    <div
+      ref="shotMe"
+      class="letter_back"
+    >
+      <p
+        :style="textFont"
+        v-html="letterText"
+      >
         {{ letterText }}
       </p>
     </div>
@@ -13,6 +19,23 @@
         max-rows="14"
         placeholder="Enter something..."
       />
+      <span>글꼴 </span>
+      <b-form-select
+        v-model="selectedFont"
+        class="mb-3"
+      >
+        <b-form-select-option
+          v-for="(font,idx) in allFont"
+          :key="idx"
+          :value="font.val"
+          :style="font.textStyle"
+        >
+          {{ font.name }}
+        </b-form-select-option>
+      </b-form-select>
+      <b-button @click="screenshot">
+        다운로드
+      </b-button>
     </div>
   </div>
 </template>
@@ -26,31 +49,84 @@ export default {
   data() {
     return {
       letter: '',
+      selectedFont: '',
+      selectedFontSize: 10,
+      allFont: [
+        { name: '기본', val: '', textStyle: '' },
+        { name: '배민 주아체', val: 'BMJUA', textStyle: 'font-family: "BMJUA"' },
+        { name: '귀여움 기본', val: 'CuteFont-Regular', textStyle: 'font-family: "CuteFont-Regular' },
+        { name: '나눔 브러쉬', val: 'NanumBrushScript-Regular', textStyle: 'NanumBrushScript-Regular' },
+        { name: '나눔 펜', val: 'NanumPenScript-Regular', textStyle: 'font-family: "NanumPenScript-Regular"' },
+        { name: '하루체', val: 'SingleDay-Regular', textStyle: 'font-family: "SingleDay-Regular"' },
+      ],
     };
   },
   computed: {
     letterText() {
       return this.letter.replace(/(?:\r\n|\r|\n)/g, '<br />');
     },
+    textFont() {
+      return `font-family: ${this.selectedFont};`;
+    },
+  },
+  methods: {
+    async screenshot() {
+      const el = this.$refs.shotMe;
+      // add option type to get the image version
+      // if not provided the promise will return
+      // the canvas.
+      const options = {
+        type: 'dataURL',
+        width: '700px',
+        height: '980px',
+      };
+      const output = await this.$html2canvas(el, options);
+
+      const a = document.createElement('a');
+      a.setAttribute('download', 'AllOfLetter.png');
+      a.setAttribute('href', output);
+      a.click();
+    },
   },
 };
 </script>
 <style>
+@font-face {
+  font-family: 'BMJUA';
+  src: url('../assets/fonts/BMJUA.ttf');
+}
+@font-face {
+  font-family: 'CuteFont-Regular';
+  src: url('../assets/fonts/CuteFont-Regular.ttf');
+}
+@font-face {
+  font-family: 'NanumBrushScript-Regular';
+  src: url('../assets/fonts/NanumBrushScript-Regular.ttf');
+}
+@font-face {
+  font-family: 'NanumPenScript-Regular';
+  src: url('../assets/fonts/NanumPenScript-Regular.ttf');
+}
+@font-face {
+  font-family: 'SingleDay-Regular';
+  src: url('../assets/fonts/SingleDay-Regular.ttf');
+}
 .letter_container{
   margin-top: 60px;
+  display: flex;
 }
 .letter_back{
   width: 500px;
+  min-width: 500px;
   height: 700px;
   background-image: url('../assets/images/letter-1.jpg');
-  display: inline-block
 }
 .letter_write{
   width: 500px;
+  min-width: 200px;
   height: 700px;
-  display: inline-block;
-  float: right;
   margin-top: 70px;
+  margin-left: 100px;
 }
 .letter_back > p{
   position: relative;
